8d7eca401eabf69756049ff2453dc81cf1ec69c9
Lukeguy334 <lhs57@usf.edu>
2025-03-02 15:52:23 -0500

    Debugg

--- cc_12.js
+++ cc_12.js
@@ -8,23 +8,6 @@ if (dashboardById === dashboardBySelector) {
   console.log('Same element selected');
 }
 
-//Create a new div element
-const revenueCard = document.createElement('div');
-revenueCard.setAttribute('class', 'metric-card');
-revenueCard.setAttribute('id', 'revenueCard');
-
-const title = document.createElement('h3');
-title.textContent = 'Revenue';
-
-const value = document.createElement('p');
-value.textContent = '$0';
-
-revenueCard.appendChild(title);
-revenueCard.appendChild(value);
-
-    dashboardById.appendChild(revenueCard);
-    console.log('Revenue card added to the dashboard');
-
 // Task 2
 const metricCards = document.querySelectorAll('.metric-card');
 
